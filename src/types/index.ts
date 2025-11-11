5eb4e59e8d474ebcc3c299698991c9fa7e99be9b
Take <134905828+Takeroh@users.noreply.github.com>
2025-11-11 12:51:41 +0000

    chore: initial commit

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+export type User = {
+    id: number;
+    name: string;
+    email: string;
+};
+
+export type Post = {
+    id: number;
+    title: string;
+    content: string;
+    authorId: number;
+};
+
+export interface ApiResponse<T> {
+    data: T;
+    error?: string;
+}
